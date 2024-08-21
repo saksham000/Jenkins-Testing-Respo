bc267c0458c87880fbb779cf49e119b54245ae5a
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-08-21 21:37:19 +0530

    initial commit

---
+++ src/test/java/com/jenkins/practice/PracticeApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jenkins.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(PracticeApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test is Executed...!");
+		assertEquals(true, true);
+	}
+
+}
